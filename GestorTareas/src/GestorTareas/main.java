6181f8bd57e177c764c57e50474de8bdb15a95a6
Estela <boxbelmonteestela@gmail.com>
2025-04-08 21:03:19 +0200

    código integrado en main

--- GestorTareas/src/GestorTareas/main.java
+++ GestorTareas/src/GestorTareas/main.java
@@ -1,11 +1,35 @@
 package GestorTareas;
+import java.util.Scanner;
 
 public class main {
+	 public static void main(String[] args) {
+		 
+	 GestorTareas gestor = new GestorTareas();
+	 MostrarTareas mostrar = new MostrarTareas();
+	 Scanner scanner = new Scanner(System.in);
+	 
+	 while (true) {
+	 System.out.println("\n1. Agregar tarea");
+	 System.out.println("2. Listar tareas");
+	 System.out.println("3. Salir");
+	 System.out.print("Seleccione una opción: ");
 
-	public static void main(String[] args) {
-		System.out.println("Bienvenido al Gestor de Tareas");
-		 // Aquí se llamarán las funcionalidades implementadas por cada integrante
-
+	 int opcion = scanner.nextInt();
+	 scanner.nextLine(); // Limpiar buffer
+	 switch (opcion) {
+	 case 1:
+	 gestor.agregarTarea();
+	 break;
+	 case 2:
+	 mostrar.listarTareas(gestor);
+	 break;
+	 case 3:
+	 System.out.println("Saliendo...");
+	 return;
+	 default:
+	 System.out.println("Opción no válida.");
+	 }
+	 
 	}
-
+}
 }
