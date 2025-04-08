8d8512eb725cca4f68b508214840033fc6606cbb
Estela <boxbelmonteestela@gmail.com>
2025-04-08 20:07:14 +0200

    He creado un nuevo proyecto Java att Estela

---
+++ GestorTareas/src/GestorTareas/main.java
@@ -0,0 +1,11 @@
+package GestorTareas;
+
+public class main {
+
+	public static void main(String[] args) {
+		System.out.println("Bienvenido al Gestor de Tareas");
+		 // Aquí se llamarán las funcionalidades implementadas por cada integrante
+
+	}
+
+}
